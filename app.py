216b54458dab3b41612c99f0aaa291e65b979479
Ved <ved00684@gmail.com>
2026-05-02 21:36:29 +0530

    Full code complete

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-from templates.models import db, Header_model
+from templates.models import Header_model,Description_model,Project_model,Work_model,web_link_model
+from templates.models import db
+
 
 app = Flask(__name__)
 
@@ -31,20 +33,105 @@ def detail():
         db.session.add(Resume_header)
         db.session.commit()
         
-        # Use Template_id (the primary key from your model)
-        # return redirect(url_for("Template", header_id=Resume_header.Template_id))
         return redirect(url_for("Template", header_id=Resume_header.Template_id))
     
     return render_template("detail.html")
 
-@app.route("/Template/<int:header_id>")
+
+
+@app.route("/Template/<int:header_id>",methods = ["GET","POST"])
 def Template(header_id):
     headers = Header_model.query.get_or_404(header_id)
+    if request.method == "POST":
+        User_Description = request.form["User_Description"]
+        Resume_description = Description_model(
+        User_Description = User_Description
+        )
+        db.session.add(Resume_description)
+        db.session.commit()
+        return redirect(url_for("body_temp",header_id=Resume_description.description_id))
     return render_template("template_body.html", headers=headers)
 
-@app.route("/app")
-def index():
-    return render_template("index1.html")
 
+
+
+@app.route("/Template_body/<int:header_id>",methods = ["GET","POST"])
+def body_temp(header_id):
+    headers = Description_model.query.get_or_404(header_id)
+    if request.method == "POST":
+        Project_name1 = request.form["Project_name1"]
+        Project_name2 = request.form["Project_name2"]
+        Project_description1 = request.form["Project_description1"]
+        Project_description2 = request.form["Project_description2"]
+        
+        Resume_project = Project_model(
+            Project_name1 = Project_name1,
+            Project_name2 = Project_name2,
+            Project_description1 = Project_description1,
+            Project_description2 = Project_description2
+        )
+        db.session.add(Resume_project)
+        db.session.commit()
+        return redirect(url_for("work_template",header_id= Resume_project.project_id))
+    return render_template("body_temp.html",headers=headers)
+
+
+
+@app.route("/temp_data/<int:header_id>",methods = ["GET","POST"])
+def work_template(header_id):
+    headers = Description_model.query.get_or_404(header_id)
+
+    if request.method == "POST":
+        Work_name1 = request.form["Work_name1"]
+        Work_name2 = request.form["Work_name2"]
+        Work_description1 = request.form["Work_description1"]
+        Work_description2 = request.form["Work_description2"]
+        
+        Resume_Work = Work_model(
+        Work_name1 = Work_name1,
+        Work_name2 = Work_name2,
+        Work_description1 = Work_description1,
+        Work_description2 = Work_description2
+        )
+        db.session.add(Resume_Work)
+        db.session.commit()
+        return redirect(url_for("link_template",header_id = Resume_Work.Work_id))
+    return render_template("workdetail_template.html",headers=headers)
+
+
+
+@app.route("/link_template/<int:header_id>",methods = ["GET","POST"])
+def link_template(header_id):
+    headers = Work_model.query.get_or_404(header_id)   
+    if request.method == "POST":
+         web_link1 = request.form["web_link1"]
+         web_link2 = request.form["web_link2"]
+         web_link3 = request.form["web_link3"]
+         
+         Resume_links = web_link_model(
+            web_link1 = web_link1,
+            web_link2 = web_link2,
+            web_link3 = web_link3
+         )
+         db.session.add(Resume_links)
+         db.session.commit()
+         return redirect(url_for("main_page",header_id = Resume_links.web_link_id))
+    return render_template("link_temp.html",headers=headers)
+
+
+@app.route("/main/<int:header_id>",methods = ["GET","POST"])
+def main_page(header_id):
+    Header= Header_model.query.get_or_404(header_id)
+    Description= Description_model.query.get_or_404(header_id)
+    Project= Project_model.query.get_or_404(header_id)
+    Work= Work_model.query.get_or_404(header_id)
+    web_link= web_link_model.query.get_or_404(header_id)
+    return render_template("main.html",
+                           Header = Header,
+                           Description = Description,
+                           Project = Project,
+                           Work = Work,
+                           web_link = web_link,
+                           )
 if __name__ == "__main__":
     app.run(debug=True, port=8001)
--- templates/models.py
+++ templates/models.py
@@ -24,3 +24,71 @@ class Header_model(db.Model):
 
     def __repr__(self):
         return f"{ self.Full_name }, { self.email }, { self.phone }"
+    
+class Description_model(db.Model):
+    __tablename__ = "Resume_description_model"
+    description_id = db.Column(db.Integer,primary_key = True)
+    User_Description = db.Column(db.String(),nullable = False)
+    
+    def __ini__(
+        self,User_Description
+    ):
+
+        self.User_Description = User_Description
+    
+class Project_model(db.Model):
+    __tablename__ = "Project_model"
+    project_id = db.Column(db.Integer,primary_key = True)
+    Project_name1 = db.Column(db.String(),nullable = False)
+    Project_name2 = db.Column(db.String(),nullable = False)
+    Project_description1 = db.Column(db.String(),nullable = False)
+    Project_description2 = db.Column(db.String(),nullable = False)
+    
+    def __init__(self,
+                Project_description1,
+                Project_description2,
+                Project_name1,
+                Project_name2,                 
+                 ):
+        self.Project_description1 =  Project_description1
+        self.Project_description2 =  Project_description2
+        self.Project_name1 =    Project_name1
+        self.Project_name2 =    Project_name2
+class Work_model(db.Model):
+    __tablename__ = "Work_model"
+    Work_id = db.Column(db.Integer,primary_key = True)
+    Work_name1 = db.Column(db.String(),nullable = False)
+    Work_name2 = db.Column(db.String(),nullable = False)
+    Work_description1 = db.Column(db.String(),nullable = False)
+    Work_description2 = db.Column(db.String(),nullable = False)
+        
+    def __init__(
+        self,
+        Work_name1,
+        Work_name2,
+        Work_description1,
+        Work_description2,
+    ):    
+        self.Work_name1 = Work_name1
+        self.Work_name2 = Work_name2
+        self.Work_description1 = Work_description1
+        self.Work_description2 = Work_description2
+            
+
+class web_link_model(db.Model):
+    __tablename__ = "web_link_model"
+    web_link_id = db.Column(db.Integer,primary_key = True)
+    web_link1 = db.Column(db.String(),nullable = False)
+    web_link2 = db.Column(db.String(),nullable = False)
+    web_link3 = db.Column(db.String(),nullable = False)
+    
+    def __init__(
+        self,
+        web_link1,
+        web_link2,
+        web_link3,
+    ):
+        self.web_link1 = web_link1
+        self.web_link2 = web_link2
+        self.web_link3 = web_link3
+    
